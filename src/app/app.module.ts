788a3a665232e965745db3d28fa8c66cce03c04b
ahsirt101197 <banua.gabrielle10@gmail.com>
2018-01-10 03:36:31 +0800

    added awesome.pipe.spec.ts, awesome.pipe.ts, modified app.componet.ts, and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
 import { HighlightDirective } from './highlight.directive';
 import { ContactService } from './contact/contact.service';
+import { ContactComponent } from './contact/contact.component';
+import { ContactHighlightDirective } from './contact/contact.highlight.directive';
+import { AwesomePipe } from './awesome.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
   AppComponent,
   HighlightDirective,
   TitleComponent,
+
+  AwesomePipe,
+  ContactComponent,
+  ContactHighlightDirective,
 ],
   imports: [ BrowserModule, FormsModule ],
   providers: [ ContactService, UserService ],
