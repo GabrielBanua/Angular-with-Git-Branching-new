14220f452c65606222346b273194783d4edc0f53
ahsirt101197 <banua.gabrielle10@gmail.com>
2018-01-10 02:36:16 +0800

    Added title.component.ts, title.component.html, user.service.spec.ts, and user.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+  AppComponent,
+  HighlightDirective,
+  TitleComponent,
+],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
