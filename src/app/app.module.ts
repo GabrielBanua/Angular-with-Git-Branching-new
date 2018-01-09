c1025c1e878f62955cce1a4fe8493aedc40ead67
ahsirt101197 <banua.gabrielle10@gmail.com>
2018-01-10 02:40:21 +0800

    modified app.module.ts importing highlight.directive to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
